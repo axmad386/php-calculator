efeefe757070fad2cfc4510bcc2f5ce758838da3
Akhmad Salafudin <axmad386@gmail.com>
2022-03-19 11:10:06 +0700

    feat: initial commit

---
+++ src/Calc.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Dmuggle\Calculator;
+
+class Calc {
+    public static function add($a, $b){
+        return $a + $b;
+    }
+
+    public static function substract($a, $b){
+        return $a - $b;
+    }
+    
+    public static function divide($a, $b){
+        return $a / $b;
+    }
+
+    public static function multiply($a, $b){
+        return $a * $b;
+    }
+}
